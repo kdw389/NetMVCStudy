9d4b3ed81f1bac62129b418d772ddf0dda32971f
kdw <dw.kim@iljin.co.kr>
2019-05-21 17:24:24 +0900

    20190521 Study

---
+++ ASPNETMVCStudy/ASPNETMVCStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETMVCStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
